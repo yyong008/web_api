fdcb247361472e7c111607279c163d0d4e31d14e
Magnesium- <757057999@qq.com>
2021-05-04 17:57:14 +0800

    PromiseLike

---
+++ base_interface/DOM/PromiseLike.md
@@ -0,0 +1,12 @@
+# PromiseLike
+
+## 简单示例
+
+```ts
+let pl: PromiseLike<any>;
+let plc: PromiseConstructorLike;
+```
+
+## 从 PromiseLike 中
+
+## 参考
--- base_struct_data/index.ts
+++ base_struct_data/index.ts
@@ -9,3 +9,7 @@ let arr: number[] = [1,3,4];
 let arr_any: any[] = [false, 1, 'string', true, {x: 'this is good'}, [23, 34], () => 123];
 let arr_new = new Array(2)
 let arr_like: ArrayLike<any> =[{1: 123}, {length: 1}]
+
+// promise like
+let pl: PromiseLike<any>;
+let plc: PromiseConstructorLike;
